957680617c0dc08f8a669da1b38c284b434f5b07
Severus7 <joshuasapz@gmail.com>
2022-04-10 21:01:08 +0800

    added scroll to top

--- src/App.js
+++ src/App.js
@@ -15,45 +15,48 @@ import ServicesWebDesign from "./pages/ServicesWebDesign";
 import ServicesSketchTo2D from "./pages/ServicesSketchTo2D";
 import Error from "./pages/Error";
 import darkTheme from "./assets/theme.js";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route
-          path="/services/real-estate-virtual-assistant"
-          element={<ServicesREVirtualAsssistant />}
-        />
-        <Route
-          path="/services/2D-floor-plan-to-3D-floor-plan"
-          element={<Services2DTo3D />}
-        />
-        <Route
-          path="/services/image-enhancement"
-          element={<ServicesImageEnhancement />}
-        />
-        <Route
-          path="/services/virtual-staging"
-          element={<ServicesVirtualStaging />}
-        />
-        <Route
-          path="/services/item-removal"
-          element={<ServicesItemRemoval />}
-        />
-        <Route
-          path="/services/sketch-to-2d-floor-plan"
-          element={<ServicesSketchTo2D />}
-        />
-        <Route
-          path="/services/web-design-and-development"
-          element={<ServicesWebDesign />}
-        />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route
+            path="/services/real-estate-virtual-assistant"
+            element={<ServicesREVirtualAsssistant />}
+          />
+          <Route
+            path="/services/2D-floor-plan-to-3D-floor-plan"
+            element={<Services2DTo3D />}
+          />
+          <Route
+            path="/services/image-enhancement"
+            element={<ServicesImageEnhancement />}
+          />
+          <Route
+            path="/services/virtual-staging"
+            element={<ServicesVirtualStaging />}
+          />
+          <Route
+            path="/services/item-removal"
+            element={<ServicesItemRemoval />}
+          />
+          <Route
+            path="/services/sketch-to-2d-floor-plan"
+            element={<ServicesSketchTo2D />}
+          />
+          <Route
+            path="/services/web-design-and-development"
+            element={<ServicesWebDesign />}
+          />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </ScrollToTop>
     </ThemeProvider>
   );
 }
